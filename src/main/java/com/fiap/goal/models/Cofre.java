305cc1d4ed0f91727d55f09e273dd002a38fdc5a
Renato Avila <renatoavila@outlook.com>
2021-11-06 17:57:25 -0300

    remoçao delete em cascata

--- src/main/java/com/fiap/goal/models/Cofre.java
+++ src/main/java/com/fiap/goal/models/Cofre.java
@@ -35,8 +35,7 @@ public class Cofre implements Serializable{
 	private Date vencimentoMeta;
     private double valorTotal;
     private double valorMeta;
-
-     
+ 
     @JsonIgnore
     private Conta conta;
     
